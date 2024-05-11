92a0bed5904801ed8cda052ffc902b6f63f301fe
lipcseie <lipcsei.e@gmail.com>
2024-05-11 10:08:38 +0200

    add InsertElementAtPosition project

--- DataStructuresandAlgorithms/DataStructuresandAlgorithms/Program.cs
+++ DataStructuresandAlgorithms/DataStructuresandAlgorithms/Program.cs
@@ -7,15 +7,7 @@ namespace DataStructuresandAlgorithms
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("\nInserting at the start of an array\n");
-            var insertingAtTheStartOfArray = new InsertingAtTheStartOfArray();
-            var array2 = insertingAtTheStartOfArray.CreateArrayWithInsertAtStart();  
-            ArrayUtilities.PrintArray(array2);
 
-            Console.WriteLine("\nInserting anywhere\n");
-            var insertingAnywhere = new InsertingAnywhere();
-            var array3 = insertingAnywhere.CreateArrayInsertAtnAnywhere();
-            ArrayUtilities.PrintArray(array3);
 
             Console.WriteLine("\nDelete the last element \n");
             var deleteThelast = new DeletionFromTheEnd();
---
+++ DataStructuresandAlgorithms/InsertElementAtPosition/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace InsertElementAtPosition
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] originalArray = { 1, 2, 3, 4, 5, 6, 7 };
+            int[] result = InsertElementAtPosition(originalArray, 99, 5);
+
+            foreach (int i in result)
+            {
+                Console.WriteLine(i);
+            }
+        }
+        public static int[] InsertElementAtPosition(int[] array, int newElement, int position)
+        {
+            int length = array.Length;
+            int[] newArray = new int[length + 1];
+
+            for (int i = 0; i < position; i++)
+            {
+                newArray[i] = array[i];
+            }
+
+            for (int i = position;i < length; i++)
+            {
+                newArray[i + 1] = array[i];
+            }
+            newArray[position] = newElement;
+            return newArray;
+        }
+    }
+}
