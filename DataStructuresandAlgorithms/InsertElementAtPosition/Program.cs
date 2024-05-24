c24543df5579dca4e86fe2122d784a57fb4b7fbc
lipcseie <lipcsei.e@gmail.com>
2024-05-24 16:04:56 +0200

    add upated insertelementatpostion

--- DataStructuresandAlgorithms/InsertElementAtPosition/Program.cs
+++ DataStructuresandAlgorithms/InsertElementAtPosition/Program.cs
@@ -1,15 +1,39 @@
-﻿namespace InsertElementAtPosition
+﻿using System.Diagnostics;
+
+namespace InsertElementAtPosition
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            int[] originalArray = { 1, 2, 3, 4, 5, 6, 7 };
-            int[] result = InsertElementAtPosition(originalArray, 99, 5);
+            string filePath = @"C:\Users\ASUS\Data-Structures-and-Algorithms\DataStructuresandAlgorithms\DataStructuresandAlgorithms\Data\8Kints.txt";
+            int[] numbers = ReadIntsFromFile(filePath);
 
-            foreach (int i in result)
+            if (numbers.Length > 0)
             {
-                Console.WriteLine(i);
+                int newElement = 99;
+                int position = 105;
+
+                Console.WriteLine($"Inserting {newElement} at position {position}...");
+
+                Stopwatch stopwatch = Stopwatch.StartNew();
+
+                int[] result = InsertElementAtPosition(numbers, newElement, position);
+
+                stopwatch.Stop();
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($"Time taken to insert the element: {stopwatch.ElapsedMilliseconds} ms");
+                Console.ResetColor();
+
+                Console.WriteLine("Array after insertion:");
+                foreach (int i in result)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            else
+            {
+                Console.WriteLine("No data available for insertion.");
             }
         }
         public static int[] InsertElementAtPosition(int[] array, int newElement, int position)
@@ -33,6 +57,21 @@
             }
             newArray[position] = newElement;
             return newArray;
+        }
+
+        public static int[] ReadIntsFromFile(string filePath)
+        {
+            try
+            {
+                string[] lines = File.ReadAllLines(filePath);
+                int[] numbers = Array.ConvertAll(lines, int.Parse);
+                return numbers;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred while reading the file: {ex.Message}");
+                return new int[0];
+            }
         }
     }
 }
