83f4a912ee755ab3fc4b043b6ee81cfee3d2505a
lipcseie <lipcsei.e@gmail.com>
2024-05-27 11:38:53 +0200

    add fixed insertionsort

--- DataStructuresandAlgorithms/InsertionSort/InsertionSortClass.cs
+++ DataStructuresandAlgorithms/InsertionSort/InsertionSortClass.cs
@@ -6,7 +6,37 @@ using System.Threading.Tasks;
 
 namespace InsertionSort
 {
+    // Stable Sorting Algorithm
     internal class InsertionSortClass
     {
+        public static void InsertionSort(int[] array)
+        {
+            for (int partIndex = 1; partIndex < array.Length; partIndex++)
+            {
+                int currentlyUnsorted = array[partIndex];
+                int i = 0;
+
+                for ( i = partIndex; i > 0 && array[i - 1] > currentlyUnsorted; i--)
+                {
+                    array[i] = array[i - 1];
+                }
+                array[i] = currentlyUnsorted;
+            }
+        }
+
+        public static int[] ReadIntsFromFile(string filePath)
+        {
+            try
+            {
+                string[] lines = File.ReadAllLines(filePath);
+                int[] numbers = Array.ConvertAll(lines, int.Parse);
+                return numbers;
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine($"An error occurred while reading the file: {ex.Message}");
+                return new int[0];
+            }
+        }
     }
 }
--- DataStructuresandAlgorithms/InsertionSort/Program.cs
+++ DataStructuresandAlgorithms/InsertionSort/Program.cs
@@ -1,10 +1,42 @@
-﻿namespace InsertionSort
+﻿using System.Diagnostics;
+using System.Globalization;
+
+namespace InsertionSort
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string filePath = @"C:\Users\ASUS\Data-Structures-and-Algorithms\DataStructuresandAlgorithms\DataStructuresandAlgorithms\Data\8Kints.txt";
+            int[] numbers = InsertionSortClass.ReadIntsFromFile(filePath);
+
+            if (numbers.Length > 0)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Array before sorting:");
+                Console.WriteLine("------------------------\n");
+                Console.ResetColor();
+                Console.WriteLine(string.Join(",", numbers));
+
+                Stopwatch stopwatch = Stopwatch.StartNew();
+
+                InsertionSortClass.InsertionSort(numbers);
+
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("\nArray after sorting:");
+                Console.WriteLine("------------------------\n");
+                Console.ResetColor();
+
+                Console.WriteLine(string.Join(",", numbers));
+
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($"Time taken to sort the array: {stopwatch.ElapsedMilliseconds} ms");
+                Console.ResetColor();
+            }
+            else
+            {
+                Console.WriteLine("No data to sort.");
+            }
         }
     }
 }
