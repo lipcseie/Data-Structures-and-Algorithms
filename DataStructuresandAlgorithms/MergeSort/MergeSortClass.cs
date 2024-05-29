75b3a8992cafc815a1e69dc0ed9a88f0d8bb53e1
lipcseie <lipcsei.e@gmail.com>
2024-05-29 14:36:46 +0200

    add MergeSort algorithm

---
+++ DataStructuresandAlgorithms/MergeSort/MergeSortClass.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MergeSort
+{
+    public class MergeSortClass
+    {
+        public static void MergeSort(int[] array)
+        {
+            int[] aux = new int[array.Length];
+
+            Sort(0, array.Length - 1);
+
+            void Sort(int low, int high)
+            {
+                if (high <= low)
+                    return;
+
+                int mid = (low + high) / 2;
+                Sort(low, mid);
+                Sort(mid + 1, high);
+                Merge(low, mid, high);
+            }
+
+            void Merge(int low, int mid, int high)
+            {
+                int i = low;
+                int j = mid + 1;
+
+                Array.Copy(array, low, aux, low, high - low + 1);
+
+                for (int k = low; k <= high; k++)
+                {
+                    if (i > mid) array[k] = aux[j++];
+                    else if (j > high) array[k] = aux[i++];
+                    else if (aux[j] < aux[i])
+                        array[k] = aux[j++];
+                    else
+                        array[k] = aux[i++];
+                }
+            }
+
+        }
+
+        public static int[] ReadIntsFromFile(string filePath)
+        {
+            try
+            {
+                string[] lines = File.ReadAllLines(filePath);
+                int[] numbers = Array.ConvertAll(lines, int.Parse);
+                return numbers;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred while reading the file: {ex.Message}");
+                return new int[0];
+            }
+        }
+    }
+}
---
+++ DataStructuresandAlgorithms/MergeSort/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Diagnostics;
+
+namespace MergeSort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"C:\Users\ASUS\Data-Structures-and-Algorithms\DataStructuresandAlgorithms\DataStructuresandAlgorithms\Data\8Kints.txt";
+            int[] numbers = MergeSortClass.ReadIntsFromFile(filePath);
+
+            if (numbers.Length > 0)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Array before sorting:");
+                Console.WriteLine("------------------------\n");
+                Console.ResetColor();
+                Console.WriteLine(string.Join(",", numbers));
+
+                Stopwatch stopwatch = Stopwatch.StartNew();
+
+                MergeSortClass.MergeSort(numbers);
+
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("\nArray after sorting:");
+                Console.WriteLine("------------------------\n");
+                Console.ResetColor();
+
+                Console.WriteLine(string.Join(",", numbers));
+
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($"Time taken to sort the array: {stopwatch.ElapsedMilliseconds} ms");
+                Console.ResetColor();
+            }
+            else
+            {
+                Console.WriteLine("No data to sort.");
+            }
+        }
+    }
+}
