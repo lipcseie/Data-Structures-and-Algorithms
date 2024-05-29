60c65e52a6308627c579695350376bc32cad0316
lipcseie <lipcsei.e@gmail.com>
2024-05-29 14:52:04 +0200

    add comments

--- DataStructuresandAlgorithms/MergeSort/MergeSortClass.cs
+++ DataStructuresandAlgorithms/MergeSort/MergeSortClass.cs
@@ -10,10 +10,11 @@ namespace MergeSort
     {
         public static void MergeSort(int[] array)
         {
-            int[] aux = new int[array.Length];
+            int[] auxiliary = new int[array.Length];
 
             Sort(0, array.Length - 1);
 
+            // 
             void Sort(int low, int high)
             {
                 if (high <= low)
@@ -30,16 +31,16 @@ namespace MergeSort
                 int i = low;
                 int j = mid + 1;
 
-                Array.Copy(array, low, aux, low, high - low + 1);
+                Array.Copy(array, low, auxiliary, low, high - low + 1);
 
                 for (int k = low; k <= high; k++)
                 {
-                    if (i > mid) array[k] = aux[j++];
-                    else if (j > high) array[k] = aux[i++];
-                    else if (aux[j] < aux[i])
-                        array[k] = aux[j++];
+                    if (i > mid) array[k] = auxiliary[j++];
+                    else if (j > high) array[k] = auxiliary[i++];
+                    else if (auxiliary[j] < auxiliary[i])
+                        array[k] = auxiliary[j++];
                     else
-                        array[k] = aux[i++];
+                        array[k] = auxiliary[i++];
                 }
             }
 
