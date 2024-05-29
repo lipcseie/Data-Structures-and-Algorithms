e6a72cc6de18df3eb22c966bcff7ae7e2f56a653
lipcseie <lipcsei.e@gmail.com>
2024-05-29 11:30:13 +0200

    add ShellSort algorithm

--- DataStructuresandAlgorithms/ShellSort/Program.cs
+++ DataStructuresandAlgorithms/ShellSort/Program.cs
@@ -1,10 +1,42 @@
-﻿namespace ShellSort
+﻿using System.Diagnostics;
+
+namespace ShellSort
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string filePath = @"C:\Users\ASUS\Data-Structures-and-Algorithms\DataStructuresandAlgorithms\DataStructuresandAlgorithms\Data\8Kints.txt";
+            int[] numbers = ShellShortClass.ReadIntsFromFile(filePath);
+
+            if (numbers.Length > 0)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Array before sorting:");
+                Console.WriteLine("------------------------\n");
+                Console.ResetColor();
+                Console.WriteLine(string.Join(",", numbers));
+
+                Stopwatch stopwatch = Stopwatch.StartNew();
+
+                ShellShortClass.ShellShort(numbers);
+
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("\nArray after sorting:");
+                Console.WriteLine("------------------------\n");
+                Console.ResetColor();
+
+                Console.WriteLine(string.Join(",", numbers));
+
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($"Time taken to sort the array: {stopwatch.ElapsedMilliseconds} ms");
+                Console.ResetColor();
+            }
+            else
+            {
+                Console.WriteLine("No data to sort.");
+            }
         }
+
     }
 }
---
+++ DataStructuresandAlgorithms/ShellSort/ShellShortClass.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShellSort
+{
+    public class ShellShortClass
+    {
+        public static void ShellShort(int[] array)
+        {
+            int gap = 1;
+            while (gap < array.Length / 3)
+            {
+                gap = 3 * gap + 1;
+            }
+
+            while (gap >= 1)
+            {
+                for (int i = gap; i < array.Length; i++)
+                {
+                    for (int j = i; j >= gap && array[j] < array[j - gap]; j -= gap)
+                    {
+                        Swap(array, j, j - gap);
+                    }
+                }
+
+                gap /= 3;
+            }
+        }
+
+        public static void Swap(int[] array, int i, int j)
+        {
+            int temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+
+        public static int[] ReadIntsFromFile(string filePath)
+        {
+            try
+            {
+                string[] lines = File.ReadAllLines(filePath);
+                int[] numbers = Array.ConvertAll(lines, int.Parse);
+                return numbers;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred while reading the file: {ex.Message}");
+                return new int[0];
+            }
+        }
+    }
+}
