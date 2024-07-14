b7f9109455e4a5b0066a8ca2206f8030f4208efc
lipcseie <lipcsei.e@gmail.com>
2024-07-14 13:15:13 +0200

    add DeleteTheLastElementTest

---
+++ DataStructuresandAlgorithms/DSA.Test/DeleteTheLastElementTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DeleteTheLastElement;
+
+namespace DSA.Test
+{
+    public class DeleteTheLastElementTest
+    {
+        [Fact]
+        [Trait("Category", "ArrayTests")]
+        public void InsertElementAtPosition_ShouldInsertElement()
+        {
+            // Arrange
+            int[] originalArray = { 1, 2, 3 };
+            int[] expcetedArray = { 1, 2};
+
+            // Act
+            int[] result = Program.DeleteTheLastElement(originalArray);
+
+
+            // Assert
+
+            Assert.Equal(expcetedArray, result);
+            Assert.Equal(originalArray.Length - 1, result.Length);
+        }
+    }
+}
--- DataStructuresandAlgorithms/DeleteTheLastElement/Program.cs
+++ DataStructuresandAlgorithms/DeleteTheLastElement/Program.cs
@@ -1,6 +1,6 @@
 ﻿namespace DeleteTheLastElement
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
