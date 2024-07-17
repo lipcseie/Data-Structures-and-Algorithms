36beeb51c4311206f6f002407ec33238be63c587
lipcseie <lipcsei.e@gmail.com>
2024-07-17 14:34:51 +0200

    add deletethefirstelemet shouldremoveelmeent test

---
+++ DataStructuresandAlgorithms/DSA.Test/DeleteTheFirstElementTest.cs
@@ -0,0 +1,25 @@
+﻿using DeleteTheFirstElement;
+
+namespace DSA.Test
+{
+    public class DeleteTheFirstElementTest
+    {
+        [Fact]
+        [Trait("Category", "DeleteTheFirstElementTest")]
+        public void DeleteTheFirstElement_ShouldRemoveElement()
+        {
+            // Arrange
+            int[] originalArray = { 1, 2, 3 };
+            int[] expcetedArray = { 2 ,3};
+
+            // Act
+            int[] result = Program.DeleteTheFirstElement(originalArray);
+
+
+            // Assert
+
+            Assert.Equal(expcetedArray, result);
+            Assert.Equal(originalArray.Length - 1, result.Length);
+        }
+    }
+}
--- DataStructuresandAlgorithms/DeleteTheFirstElement/Program.cs
+++ DataStructuresandAlgorithms/DeleteTheFirstElement/Program.cs
@@ -1,6 +1,6 @@
 ﻿namespace DeleteTheFirstElement
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
