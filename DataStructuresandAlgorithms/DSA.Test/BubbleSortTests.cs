509c39bfbe7618636f8581fe84dd174bf61628b0
lipcseie <lipcsei.e@gmail.com>
2024-07-19 16:27:33 +0200

    add BubbleSort_SortsArrayCorrectly test

---
+++ DataStructuresandAlgorithms/DSA.Test/BubbleSortTests.cs
@@ -0,0 +1,22 @@
+﻿using BubbleSort;
+
+namespace DSA.Test
+{
+    public class BubbleSortTests
+    {
+        [Fact]
+        [Trait("Category", "BubbleSortTests")]
+        public void BubbleSort_SortsArrayCorrectly()
+        {
+            // Arrange
+            int[] input = { 5, 2, 9, 1, 5, 6 };
+            int[] expectedOutput = { 1, 2, 5, 5, 6, 9 };
+
+            // Act
+            BubbleSortClass.BubbleSort(input);
+
+            // Assert
+            Assert.Equal(expectedOutput, input);
+        }
+    }
+}
