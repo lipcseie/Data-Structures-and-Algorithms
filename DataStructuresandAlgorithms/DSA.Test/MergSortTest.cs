6aef0089736ea2923c8da684f415ef639743912f
Edit Lipcsei <lipcsei.e@gmail.com>
2024-07-26 15:57:29 +0200

    add  mergeSort_SortsArrayCorrectly test

---
+++ DataStructuresandAlgorithms/DSA.Test/MergSortTest.cs
@@ -0,0 +1,27 @@
+﻿using ShellSort;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.Test
+{
+    public class MergSortTest
+    {
+        [Fact]
+        [Trait("Category", "MergSortTest")]
+        public void mergeSort_SortsArrayCorrectly()
+        {
+            // Arrange
+            int[] input = { 5, 2, 9, 1, 5, 6 };
+            int[] expectedOutput = { 1, 2, 5, 5, 6, 9 };
+
+            // Act
+            ShellSortClass.ShellSort(input);
+
+            // Assert
+            Assert.Equal(expectedOutput, input);
+        }
+    }
+}
