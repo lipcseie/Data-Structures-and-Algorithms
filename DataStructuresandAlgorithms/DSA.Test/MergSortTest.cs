3e3bfd2e3b5726db00655c7eda3ba4455cd08cd9
Edit Lipcsei <lipcsei.e@gmail.com>
2024-07-28 16:21:28 +0200

    add QuickSort_SortsArrayCorrectly test

--- DataStructuresandAlgorithms/DSA.Test/MergSortTest.cs
+++ DataStructuresandAlgorithms/DSA.Test/MergSortTest.cs
@@ -1,10 +1,4 @@
 ﻿using MergeSort;
-using ShellSort;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DSA.Test
 {
---
+++ DataStructuresandAlgorithms/DSA.Test/QuickSortTest.cs
@@ -0,0 +1,22 @@
+﻿using QuickSort;
+
+namespace DSA.Test
+{
+    public class QuickSortTest
+    {
+        [Fact]
+        [Trait("Category", "QuickTest")]
+        public void QuickSort_SortsArrayCorrectly()
+        {
+            // Arrange
+            int[] input = { 5, 2, 9, 1, 5, 6 };
+            int[] expectedOutput = { 1, 2, 5, 5, 6, 9 };
+
+            // Act
+            QuickSortClass.QuickSort(input);
+
+            // Assert
+            Assert.Equal(expectedOutput, input);
+        }
+    }
+}
