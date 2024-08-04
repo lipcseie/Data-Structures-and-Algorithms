aac6780a19e83b1bf25d24bdc789c10dfc16b015
Edit Lipcsei <lipcsei.e@gmail.com>
2024-08-04 19:25:13 +0200

    add QuickSort_ArrayWithNegativeNumbers_SortsCorrectly test

--- DataStructuresandAlgorithms/DSA.Test/QuickSortTest.cs
+++ DataStructuresandAlgorithms/DSA.Test/QuickSortTest.cs
@@ -79,5 +79,20 @@ namespace DSA.Test
             // Assert
             Assert.Equal(expetedOutput, input);
         }
+
+        [Fact]
+        [Trait("Category", "QuickSortTests")]
+        public void QuickSort_ArrayWithNegativeNumbers_SortsCorrectly()
+        {
+            // Arrange
+            int[] input = { -3, -1, -2, 0, 2, 1 };
+            int[] expetedOutput = { -3, -2, -1, 0, 1, 2 };
+
+            // Act
+            QuickSortClass.QuickSort(input);
+
+            // Assert
+            Assert.Equal(expetedOutput, input);
+        }
     }
 }
